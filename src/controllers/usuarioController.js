7ae0c312e4a19f70161e639829ae09404d931c19
Rafael Brandao <rbs13bg13@gmail.com>
2025-02-04 16:12:40 -0300

    Ok

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
-import { cadatrarUsuario, deletarUsuario, updateUsuario, verificarUsuario } from "../services/UsuarioService.js";
+import { cadatrarUsuario, deletarUsuario, updateUsuario, verificarUsuario } from "../services/usuarioService.js";
 
 dotenv.config();
 
