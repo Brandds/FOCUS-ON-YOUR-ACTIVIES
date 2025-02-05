264d70c639dcbc11cd92679c0fd02e11d6c85433
Rafael Brandao <rbs13bg13@gmail.com>
2025-02-05 01:57:11 -0300

    Tratamento dos campos email e senha

--- public/js/index.js
+++ public/js/index.js
@@ -8,12 +8,60 @@ const input_email = document.getElementById("email")
 const input_senha = document.getElementById("senha")
 
 btn_entrar.addEventListener("click", () => {
-  alert("teste");
-  verificarUsuario()
+  const email = validarEmail(input_email.value)
+  const senha = validarSenha(input_senha.value)
+
+  if(email && senha) verificarUsuario(input_email.value,input_senha.value)
 });
 
-async function verificarUsuario() {
-  alert("chamou");
-  const dadosUsuario = await login("teste@hotmail.com","senha123"); // Aguarda a resposta da API
-  console.log("Dados do usuário:", dadosUsuario);
+async function verificarUsuario(email, senha) {
+  const dadosUsuario = await login(email,senha); // Aguarda a resposta da API
+
+  dadosUsuario.success ? dadosCorretos() : dadosIncorretos();
+  
+}
+
+const validarEmail = (email) => {
+  const situacao  = email.includes("@");
+  modificarCampoEmail(situacao)
+  return situacao
+};
+
+function modificarCampoEmail(situacao){
+  if(situacao){
+    input_email.style.border = ""
+    document.querySelector(".msg_email").style.display = "none"
+  }else{
+    input_email.style.border = '2px solid red'
+    document.querySelector(".msg_email").style.display = "block"
+  }
+
+
+}
+
+const validarSenha = (senha) =>{
+ const situacao = senha != ""
+ modificarCampoSenha(situacao)
+ return situacao
+}
+
+function modificarCampoSenha(situacao){
+  if(situacao){
+    input_senha.style.border = ""
+    document.querySelector(".msg_senha").style.display = "none"
+  }else{
+    input_senha.style.border = "2px solid red"
+    document.querySelector(".msg_senha").style.display = "block"
+
+  }
+}
+
+function dadosCorretos(){
+  alert("Dados ok")
+  window.location.href = "pagina_inicial.html";
+
+}
+
+function dadosIncorretos(){
+  alert("Dados not")
 }
--- public/js/services/usuarioService.js
+++ public/js/services/usuarioService.js
@@ -1,5 +1,5 @@
 export async function login(email, senha) {
-  alert("entrei login")
+  let response400;
   try {
     const response = await fetch("https://focus-on-your-activies.onrender.com/usuario/verificar", {
       method: "POST",
@@ -15,11 +15,13 @@ export async function login(email, senha) {
     if (!response.ok) {
       throw new Error(`Erro na requisição: ${response.status}`);
     }
-
+    console.log(email);
+    console.log(senha);
+    
     const data = await response.json();
-    console.log("Resposta:", data);
-    return data.token
+    return data  
   } catch (error) {
     console.error("Erro:", error);
+    return ({success: false})
   }
 }
